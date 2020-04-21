2bb02d18099569f7fceef23ffd61178846e478c8
mahi_uddin <mahi.ewu@gmail.com>
2020-04-21 17:15:29 +0800

    display the notices,remarks and some parts of entites

--- index.js
+++ index.js
@@ -1,143 +1,119 @@
 window.onload = function(){
     // initiatize the UI
     const ui = new UI; 
-    //if (!document.getElementById) document.write('<link rel="stylesheet" type="text/css" href="styles.css">');
     
+    // execute this function when click the search button
     const submitButton = () => {
-        //document.getElementById("btnSubmit").onclick=function(){
-            // for scrolling
-            // let _height = document.documentElement.clientHeight;
-            // //let _height = document.body.clientHeight;
-            // //console.log(_height);
-            // document.querySelector('.container').style.height = _height+'px';
-    
-            let ip      = document.getElementById("txtIP").value;
-            // let ip      = input;
-            // let _domain = document.getElementById("txtDomain").value;
-            
-            const checkValidIP = ip =>{
-                let blocks = ip.split(".");
-                if(blocks.length === 4) {
-                    return blocks.every(function(block) {
-                    return parseInt(block,10) >=0 && parseInt(block,10) <= 255;
-                    });
-                }
-                return false;
+        // get the input value    
+        let ip      = document.getElementById("txtIP").value;
+                        
+        // check the validity of ipv4 
+        const checkValidIP = ip =>{
+            let blocks = ip.split(".");
+            if(blocks.length === 4) {
+                return blocks.every(function(block) {
+                return parseInt(block,10) >=0 && parseInt(block,10) <= 255;
+                });
             }
-    
-            const checkValidDomain = _domain =>{
-                var re = new RegExp(/^((?:(?:(?:\w[\.\-\+]?)*)\w)+)((?:(?:(?:\w[\.\-\+]?){0,62})\w)+)\.(\w{2,6})$/); 
-                return _domain.match(re);
-            }
+            return false;
+        }
 
-            const checkValidIPV6 = ip =>{
-                var re = new RegExp(/((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/); 
-                return ip.match(re);
-            }
+        // check the vality of IPv6
+        const checkValidIPV6 = ip =>{
+            var re = new RegExp(/((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/); 
+            return ip.match(re);
+        }
+    
+        // check the valid domain name
+        const checkValidDomain = _domain =>{
+            var re = new RegExp(/^((?:(?:(?:\w[\.\-\+]?)*)\w)+)((?:(?:(?:\w[\.\-\+]?){0,62})\w)+)\.(\w{2,6})$/); 
+            return _domain.match(re);
+        }
+
+        // fetch the ip address of a specific domain
+        async function getIPofDomain(_domain){
+            let response = await fetch('https://dns.google/resolve?name='+_domain);
+            let json = await response.json();
+            return json.Answer[0].data;
+        }
         
-            async function getIPofDomain(_domain){
-                let response = await fetch('https://dns.google/resolve?name='+_domain);
-                let json = await response.json();
-                return json.Answer[0].data;
-            }
-            
-            async function getApiData(url){
-                console.log(url);
-                const response = await fetch(url);
-                const jsondata = await response.json();
-                return jsondata;                    
-            }
+        // fetch the rdap information of a specific ip
+        async function getApiData(url){
+            console.log(url);
+            const response = await fetch(url);
+            const jsondata = await response.json();
+            return jsondata;                    
+        }
 
-            // fetch the ASN by passing the IP address
-            async function getASN(ip){
-                const response = await fetch(`https://ipapi.co/${ip}/json/`);
-                const data = await response.json();
-                return data.asn;    
-            }
+        // fetch the ASN by passing the IP address
+        async function getASN(ip){
+            const response = await fetch(`https://ipapi.co/${ip}/json/`);
+            const data = await response.json();
+            return data.asn;    
+        }
+        
+        // fetch the nameserver of a domain
+        async function getNameServer(_domain){
+            const response = await fetch(`https://www.whoisxmlapi.com/whoisserver/DNSService?apiKey=at_kIEcsLxf9NAlDXcPdTXOXudneUApQ&domainName=${_domain}&type=2&outputFormat=JSON`);                
+            const data = await response.json();
+            return data;    
+        }
             
-            // fetch the nameserver of a domain
-            async function getNameServer(_domain){
-                const response = await fetch(`https://www.whoisxmlapi.com/whoisserver/DNSService?apiKey=at_kIEcsLxf9NAlDXcPdTXOXudneUApQ&domainName=${_domain}&type=2&outputFormat=JSON`);                
-                const data = await response.json();
-                return data;    
-            }
-            
-            if(!ip == ""){
-                const ipValidityStatus = checkValidIP(ip);
-                const ip6ValidityStatus = checkValidIPV6(ip);
-                let asn ='';
-                if(ipValidityStatus || ip6ValidityStatus){
-                    // fetch the asn and display
-                    getASN(ip)
-                    .then(data=>{
-                          //ui.displayASN(data);
-                          asn = data; 
-                    });
-                    getApiData('https://rdap.db.ripe.net/ip/'+ip)
-                    .then(res => {
-                        ui.displayResult(res);      
-                        // let ipInfo ='';
+        if(!ip == "")
+        {
+            const ipValidityStatus = checkValidIP(ip);
+            const ip6ValidityStatus = checkValidIPV6(ip);
+            let asn ='';
+            if(ipValidityStatus || ip6ValidityStatus){
+                // fetch the asn and display
+                getASN(ip)
+                .then(data=>{
+                        //ui.displayASN(data);
+                        asn = data; 
+                });
+                getApiData('https://rdap.db.ripe.net/ip/'+ip)
+                .then(res => {
+                    ui.displayResult(res);
+                    })
+                .catch(err => {
+                        // ui.displayResult(err);
+                        document.getElementById("result").innerHTML = err;
+                })
+            }else if(checkValidDomain(ip)){
+                
+                // fetch the domain nameserver
+                getNameServer(ip)
+                    .then(res=>{
                         // this.console.log(res);
-                        // ipInfo +=`<table class='table table-hover'><thead><tr class='table-active'><td>key</td><td>Value</td></tr></thead><tbody>
-                        // <tr><td>ASN</td><td>${asn}</td></tr>`;
-                        // for (var key of Object.keys(res)) {
-                        //     ipInfo +="<tr><td>"+key + " </td><td>" + res[key] + "</td></tr>"; 
-                        //     //console.log(key + " -> " + p[key])
-                        // }
-                        // ipInfo +="</tbody></table>";
-    
-                        // document.getElementById("showIP").innerHTML = ipInfo;
+                        const nsrecords = res.DNSData.dnsRecords; 
+                        this.console.log(nsrecords[0].additionalName);
+                        });    
+                
+                // fetch the IP inforamtion by the domain IP
+                getIPofDomain(ip)
+                .then(res => {
+                    getASN(res)
+                    .then(data=>{
+                        //console.log(data);
+                        asn = data; 
+                    });
+                    
+                    getApiData('https://rdap.db.ripe.net/ip/'+res)
+                    .then(res => {
+                        ui.displayResult(res);
+                        
                         })
                     .catch(err => {
-                        // ui.displayResult(err);
-                         document.getElementById("result").innerHTML = err;
-                    })
-                }else if(checkValidDomain(ip)){
-                    
-                    // fetch the domain nameserver
-                    getNameServer(ip)
-                        .then(res=>{
-                            // this.console.log(res);
-                            const nsrecords = res.DNSData.dnsRecords; 
-                            this.console.log(nsrecords[0].additionalName);
-                         });    
-                   
-                    // fetch the IP inforamtion by the domain IP
-                    getIPofDomain(ip)
-                    .then(res => {
-                        getASN(res)
-                        .then(data=>{
-                            //console.log(data);
-                            asn = data; 
-                        });
-                        
-                        getApiData('https://rdap.db.ripe.net/ip/'+res)
-                        .then(res => {
-                            ui.displayResult(res);
-                            // this.console.log(res);
-                                  
-                            // let ipInfo ='';
-                            // ipInfo +=`<table class='table table-hover'><thead><tr class='table-active'><td>key</td><td>Value</td></tr><thead><tbody>
-                            // <tr><td>ASN</td><td>${asn}</td></tr>`;
-                            // for (var key of Object.keys(res)) {
-                            //     ipInfo +="<tr><td>"+key + " </td><td>" + res[key] + "</td></tr>"; 
-                            //     //console.log(key + " -> " + p[key])
-                            // }
-                            // ipInfo +="</table>";
-        
-                            // document.getElementById("showIP").innerHTML = ipInfo;
-                            })
-                        .catch(err => {
-                            ui.displayResult(res);
-                            document.getElementById("result").innerHTML = err;
-                        })                
-                    }) 
-                }else{
-                    document.getElementById("showIP").innerHTML = "Invalid IP / Domain "+ip;
-                }
-    
+                        ui.displayResult(res);
+                        document.getElementById("result").innerHTML = err;
+                    })                
+                }) 
+            }else{
+                document.getElementById("showIP").innerHTML = "Invalid IP / Domain " + ip;
             }
+    
         }
+    }
     
     var input      = document.getElementById("txtIP");
     //let inputValue = document.getElementById("txtIP").value;
--- ui.js
+++ ui.js
@@ -17,40 +17,177 @@ class UI{
     displayResult(res){
         console.log(res);
         let ip = document.getElementById('txtIP').value;
-        let startAddress;
-        let endAddress;
-        let handle;
-        let parentHandle;
-        let name;
-        let type;
-        let port43;
-        let version;
-        let country;
+        let startAddress, endAddress, handle, parentHandle='Not Found', name;
+        let type, port43, version, country='Not Found';
+        let notices;
+        let noticeOutput;
+        let remarks, remarkOutput;
+        let entities, entityOutput;
+        let events, eventsOutput;
+        let links, linksOutput;
+
         // iterate the value of the ip
         for (var key of Object.keys(res)) {
-            if(key == 'handle'){
-                handle = res[key]; 
-            }else if(key == 'parentHandle'){
-                parentHandle = res[key]; 
-            }else if(key == 'name'){
-                name = res[key]; 
-            }else if(key == 'type'){
-                type = res[key]; 
-            }else if(key == 'startAddress'){
-                startAddress = res[key]; 
-            }else if(key == 'ipVersion'){
-                version = res[key]; 
-            }else if(key == 'endAddress'){
-                endAddress = res[key]; 
-            }else if(key == 'port43'){
-                port43 = res[key]; 
-            }else if(key == 'country'){
-                country = res[key]; 
+            
+            //const expr = key;
+            switch (key) {
+            case 'handle':
+                handle = res[key];
+                break;
+            case 'parentHandle':
+                parentHandle = res[key];
+                break;
+            case 'name':
+                name = res[key];
+                break;
+            case 'type':
+                type = res[key];
+                break;
+            case 'startAddress':
+                startAddress = res[key];
+                break;
+            case 'ipVersion':
+                version = res[key];
+                break;
+            case 'endAddress':
+                endAddress = res[key];
+                break;
+            case 'port43':
+                port43 = res[key];
+                break;
+            case 'country':
+                country = res[key];
+                break;
+            case 'notices':
+                notices = res[key];
+                notices.forEach(element => {
+                    
+                    
+                    //var obj = { first: "John", last: "Doe" };
+                    // Visit non-inherited enumerable keys
+                    let notice;
+                    Object.keys(element).forEach(function(notice) {
+                        switch (notice) {
+                            case 'title':
+                                noticeOutput += '<br><b>' + element[notice] + '</b>';
+                                break;
+                            case 'description':
+                                noticeOutput += '<br>' + element[notice][0] + '<br>';
+                                break;
+                            default:    
+                        }
+                    });
+                     
+                });
+                
+                break;
+            case 'remarks':
+                remarks = res[key];
+                remarks.forEach(element => {
+                
+                    // Visit non-inherited enumerable keys
+                    let remark;
+                    Object.keys(element).forEach(function(remark) {
+                        switch (remark) {
+                            case 'description':
+                                remarkOutput += '<br>' + element[remark][0] + '<br>';
+                                break;
+                            case 'title':
+                                remarkOutput += '<br><b>' + element[remark] + '</b>';
+                                break;
+                            
+                            default:    
+                        }
+                    });
+                        
+                });
+                
+                break;
+            case 'entities':
+                entities = res[key];
+                let entityNumber=0;
+                entities.forEach(element => {
+                    entityNumber++;
+                    entityOutput +="<hr><h5>Entity Number: "+entityNumber+"<h5>";
+                    // Visit non-inherited enumerable keys
+                    let entity;
+                    Object.keys(element).forEach(function(entity) {
+                        switch (entity) {
+                            case 'events':
+                                events = element[entity];
+                                eventsOutput='';
+                                //console.log(events.length);
+                                events.forEach(element => {
+                                    //console.log('i am in event');
+                                    // Visit non-inherited enumerable keys
+                                    let event;
+                                    Object.keys(element).forEach(function(event) {
+                                        switch (event) {
+                                            case 'eventAction':
+                                                eventsOutput += '<br><b>' + element[event] + '<b>';
+                                                break;
+                                            case 'eventDate':
+                                                eventsOutput += '<br>' + element[event];
+                                                break;
+                                            
+                                            default:    
+                                        }
+                                    });
+                                        
+                                });
+
+                                entityOutput += "<br>Event list"+ eventsOutput;  
+                                break;
+                            
+                            case 'links':
+                                links = element[entity];
+                                linksOutput='';
+                                //console.log(events.length);
+                                links.forEach(element => {
+                                    //console.log('i am in event');
+                                    // Visit non-inherited enumerable keys
+                                    let event;
+                                    Object.keys(element).forEach(function(link) {
+                                        switch (link) {
+                                            case 'href':
+                                                linksOutput += '<br><b>href: ' + element[link] + '<b>';
+                                                break;
+                                            case 'rel':
+                                                linksOutput += '<br>rel: ' + element[link];
+                                                break;
+                                            case 'type':
+                                                linksOutput += '<br>type: ' + element[link];
+                                                break;
+                                            case 'value':
+                                                linksOutput += '<br>value: ' + element[link];
+                                                break;        
+                                            
+                                            default:    
+                                        }
+                                    });
+                                        
+                                });
+
+                                entityOutput += "<br>Link list<br>"+ linksOutput;  
+                                break;    
+                            case 'handle':
+                                entityOutput += '<br><b>Handle:</b> ' + element[entity] + '<br>';
+                                break;
+                            case 'objectClassName':
+                                entityOutput += '<br><b>Object Class Name:</b>' + element[entity] + '</b>';
+                                break;
+                            
+                            default:    
+                        }
+                    });
+                        
+                });
+                
+                break;                                  
+            default:
+                
             }
-
-
-            // ipInfo +="<tr><td>"+key + " </td><td>" + res[key] + "</td></tr>"; 
-            //console.log(key + " -> " + p[key])
+                        
         }
         let output = `
         <h3>Search Result</h3>
@@ -111,6 +248,18 @@ class UI{
                       <tr>
                         <th scope="row">Port 43 Whois</th>
                         <td>${port43}</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Notices</th>
+                        <td>${noticeOutput}</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Remarks</th>
+                        <td>${remarkOutput}</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Entities</th>
+                        <td>${entityOutput}</td>
                       </tr>
                     </tbody>
                   </table>
